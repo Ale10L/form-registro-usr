5717ee8286dcfa9ff04b093c9924f3f82eb1d12a
Alejandro Ghiringhelli <alejandroghiringhelli@gmail.com>
2023-05-17 12:57:15 -0300

    agregando validaciones de fecha de nac y correo elect, almacenar en db

--- backend/repositorio/usr_repo.py
+++ backend/repositorio/usr_repo.py
@@ -2,6 +2,9 @@ from sqlalchemy import select
 from sqlalchemy.orm.session import Session
 from fastapi.exceptions import HTTPException
 from modelos.usr_model import UsrBd, UsrApi
+import re
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 class UsrRepositorio:
@@ -13,6 +16,16 @@ class UsrRepositorio:
         return usr
     
     def agregar_usr(self, datos: UsrApi, session: Session):
+        correo = datos.correo_electronico
+        expresion_regular = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+        validar_correo = re.match(expresion_regular, correo)
+        fecha_actual = datetime.now().date()
+        fec_nac = datos.fecha_nacimiento.date()
+        edad = relativedelta(fecha_actual, fec_nac)
+        if edad.years <= 17:
+            raise HTTPException(status_code=406, detail='El usuario debe ser mayor de 18 años')
+        if validar_correo is None:
+            raise HTTPException(status_code=406, detail='El formato de correo electrónico ingresado no es correcto')
         usr = UsrBd(nombre_completo = datos.nombre_completo, fecha_nacimiento = datos.fecha_nacimiento, correo_electronico = datos.correo_electronico, contraseña = datos.contraseña, genero_id = datos.genero_id, pais_id = datos.pais_id)
         session.add(usr)
         session.commit()
@@ -32,6 +45,21 @@ class UsrRepositorio:
         usr = session.get(UsrBd, id)
         if usr is None:
             raise HTTPException(status_code=404, detail='El usuario no existe')
+        fecha_actual = datetime.now().date()
+        fec_nac = datos.fecha_nacimiento.date()
+        edad = relativedelta(fecha_actual, fec_nac)
+        if edad.years <= 17:
+            raise HTTPException(status_code=406, detail='El usuario debe ser mayor de 18 años')
+        correo = datos.correo_electronico
+        expresion_regular = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+        validar = re.match(expresion_regular, correo)
+        if validar is None:
+            raise HTTPException(status_code=406, detail='El formato de correo electrónico ingresado no es correcto')
+        fecha_actual = datetime.now()
+        edad = fecha_actual - datos.fecha_nacimiento
+        if edad <= 17:
+            raise HTTPException(status_code=406, detail='El usuario debe ser mayor de 18 años')
+        
         try:
             usr.nombre_completo = datos.nombre_completo
             usr.fecha_nacimiento = datos.fecha_nacimiento
